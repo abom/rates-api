1b442a3e9a032db81be972769e87f9ac8070360e
Abdelrahman Ghanem <abom.jdev@gmail.com>
2021-08-22 06:05:56 +0200

    update cron interval of the background task

--- rateapi/app/tasks.py
+++ rateapi/app/tasks.py
@@ -5,6 +5,6 @@ from periodiq import cron
 from .helpers import fetch_and_save_rate
 
 
-@dramatiq.actor(periodic=cron("* * * * *"))
+@dramatiq.actor(periodic=cron("0 * * * *"))
 def hourly():
     fetch_and_save_rate()
